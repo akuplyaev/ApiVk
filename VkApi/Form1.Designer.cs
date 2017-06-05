d0292bdc215767b816fcc7968b9c89f59954ec36
Aleksey Kuplyaev <a.kuplyaev@gmail.com>
2017-06-05 14:12:12 +0300

    Create project

---
+++ VkApi/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace VkApi
+{
+    partial class ApiApplication
+    {
+
+
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listFriends = new System.Windows.Forms.ListBox();
+            this.btnListFriends = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(41, 45);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(171, 20);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(41, 106);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(171, 20);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Фамилия";
+            // 
+            // listFriends
+            // 
+            this.listFriends.FormattingEnabled = true;
+            this.listFriends.Location = new System.Drawing.Point(354, 45);
+            this.listFriends.Name = "listFriends";
+            this.listFriends.Size = new System.Drawing.Size(271, 342);
+            this.listFriends.TabIndex = 4;
+            // 
+            // btnListFriends
+            // 
+            this.btnListFriends.Location = new System.Drawing.Point(55, 217);
+            this.btnListFriends.Name = "btnListFriends";
+            this.btnListFriends.Size = new System.Drawing.Size(140, 23);
+            this.btnListFriends.TabIndex = 5;
+            this.btnListFriends.Text = "Получить список друзей";
+            this.btnListFriends.UseVisualStyleBackColor = true;
+            this.btnListFriends.Click += new System.EventHandler(this.btnListFriends_Click);
+            // 
+            // ApiApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 466);
+            this.Controls.Add(this.btnListFriends);
+            this.Controls.Add(this.listFriends);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ApiApplication";
+            this.Text = "ApiApplication";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listFriends;
+        private System.Windows.Forms.Button btnListFriends;
+    }
+}
+
---
+++ VkApi/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Windows.Forms;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model.RequestParams;
+
+namespace VkApi
+{
+    using VkNet;
+    using VkNet.Enums.Filters;
+
+
+
+    public partial class ApiApplication : Form
+    {
+        VkApi vk = new VkApi();       
+        public ApiApplication()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            
+            Settings scope = Settings.Friends;
+            vk.Authorize(new ApiAuthParams
+            {
+                ApplicationId = 6061372,
+                Login = "89105741622",
+                Password = "serega94",
+                Settings = Settings.All
+            });
+            var CurrentUser = vk.Users.Get(vk.UserId.Value, ProfileFields.All);
+            txtFirstName.Text = CurrentUser.FirstName;
+            txtLastName.Text = CurrentUser.LastName;
+        }
+
+        private void btnListFriends_Click(object sender, System.EventArgs e)
+        {
+            var friends = vk.Friends.Get(new FriendsGetParams
+            {
+                UserId = 137280448,                
+                Fields = ProfileFields.LastName,                
+                Order = FriendsOrder.Name,
+            });
+           
+            foreach (var friend  in friends)
+            {
+                listFriends.Items.Add(friend.FirstName+" "+ friend.LastName);
+            }
+            
+        }
+    }
+}
